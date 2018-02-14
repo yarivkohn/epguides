7a2919ea7a84fcf6da4a75fa4a9e849f34d0e2c8
ykohn <ykohn@golantelecom.co.il>
2018-02-14 16:43:26 +0200

    Code clean up

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -10,49 +10,46 @@ namespace Epguides\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Episode extends Model
-{
-	const MAX_LIFE_TIME = 60 * 60 * 6; // 6 Hrs
+class Episode extends Model {
+	const MAX_LIFE_TIME = 60*60*6; // 6 Hrs
 
 	protected $fillable = [
-		'name'                     ,
-		'show_id'                  ,
-		'last_episode_season'      ,
-		'last_episode_number'      ,
+		'name',
+		'show_id',
+		'last_episode_season',
+		'last_episode_number',
 		'last_episode_release_date',
-		'next_episode_season'      ,
-		'next_episode_number'      ,
+		'next_episode_season',
+		'next_episode_number',
 		'next_episode_release_date',
 		'sms_sent',
 	];
 
 	public function getFollowedShows() {
 		$list = [];
-		if(file_exists($this->cacheDir()) &&
-			is_readable($this->cacheDir()) &&
-			filemtime($this->cacheDir()) > time() - self::MAX_LIFE_TIME){
-			$list = json_decode(file_get_contents($this->cacheDir()));
-		} else {
-			foreach($this->getListOfShows() as $show){
-				$episodeData = $show->getAttributes();
-				$list[]  = $this->createEpisodeObject($episodeData);
-			}
-//			file_put_contents($this->cacheDir(), json_encode($list));
+		foreach ($this->getListOfShows() as $show) {
+			$episodeData = $show->getAttributes();
+			$list[]      = $this->createEpisodeObject($episodeData);
 		}
 		return $list;
 	}
 
-	/**
-	 * @return string
-	 */
-	private function cacheDir()
-	{
-		return __DIR__ . DS . '..' . DS . '..' . DS . 'resources' . DS . 'cache' . DS . 'cache.json';
-	}
 
+	/**
+	 * Return list of episodes with their data
+	 * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Support\Collection|static[]
+	 */
 	private function getListOfShows() {
-		$t = $this->whereNotNull('next_episode_release_date');
-		return $t->get();
+		return $this->whereNotNull('next_episode_release_date')
+			->join('shows', 'show_id', '=', 'id', 'left')
+			->select('episodes.name',
+				'episodes.last_episode_season',
+				'episodes.last_episode_number',
+				'episodes.last_episode_release_date',
+				'episodes.next_episode_season',
+				'episodes.next_episode_number',
+				'episodes.next_episode_release_date',
+				'shows.name as show_name')->get();
 	}
 
 	/**
@@ -60,10 +57,11 @@ class Episode extends Model
 	 * @return \stdClass
 	 */
 	private function createEpisodeObject($episodeData) {
-		$episode                         = new \stdClass();
-		$episode->lastEpisode = new \stdClass();
-		$episode->nextEpisode = new \stdClass();
+		$episode                            = new \stdClass();
+		$episode->lastEpisode               = new \stdClass();
+		$episode->nextEpisode               = new \stdClass();
 		$episode->name                      = $episodeData['name'];
+		$episode->show_name                 = $episodeData['show_name'];
 		$episode->lastEpisode->season       = $episodeData['last_episode_season'];
 		$episode->lastEpisode->number       = $episodeData['last_episode_number'];
 		$episode->lastEpisode->release_date = $episodeData['last_episode_release_date'];
@@ -71,6 +69,7 @@ class Episode extends Model
 		$episode->nextEpisode->number       = $episodeData['next_episode_number'];
 		$episode->nextEpisode->release_date = $episodeData['next_episode_release_date'];
 		$this->addDisplayDecorations($episode);
+
 		return $episode;
 	}
 
@@ -78,18 +77,19 @@ class Episode extends Model
 	/**
 	 * If episode was released in the past week, add "is_new" tag.
 	 * If episode was released more than a week ago but less than 2 weeks, add "getting_older" tag.
+	 *
 	 * @param $episode
 	 */
 	private function isNewRelease($episode) {
-    	$today = new \DateTime();
-    	$releaseDate = date_create_from_format('Y-m-d',$episode->release_date);
-		$diff = $today->diff($releaseDate);
-		if($diff->days > 0 && $diff->days < 7){
-			$episode->is_new = true;
+		$today       = new \DateTime();
+		$releaseDate = date_create_from_format('Y-m-d', $episode->release_date);
+		$diff        = $today->diff($releaseDate);
+		if ($diff->days>0 && $diff->days<7) {
+			$episode->is_new = TRUE;
 		}
 
-		if($diff->days >= 7 && $diff->days < 14){
-			$episode->is_getting_older = true;
+		if ($diff->days>=7 && $diff->days<14) {
+			$episode->is_getting_older = TRUE;
 		}
 
 
@@ -98,14 +98,15 @@ class Episode extends Model
 	/**
 	 * If release date is getting close (less than 3 days),
 	 * add "almost_released" tag.
+	 *
 	 * @param $episode
 	 */
 	private function isReleaseDateClose($episode) {
-		$today = new \DateTime();
-		$releaseDate = date_create_from_format('Y-m-d',$episode->release_date);
-		$diff = $today->diff($releaseDate);
-		if($diff->days < 3 ){
-			$episode->almost_released = true;
+		$today       = new \DateTime();
+		$releaseDate = date_create_from_format('Y-m-d', $episode->release_date);
+		$diff        = $today->diff($releaseDate);
+		if ($diff->days<3) {
+			$episode->almost_released = TRUE;
 		}
 	}
 
